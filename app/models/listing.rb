281088931be3bdd3072cd1be6ed8ecdc7a41e0b7
unknown <win.sandy@xingyi-PC.icggroup.local>
2014-11-04 18:13:31 +0800

    Validate the user in user model but need to add the link in rout.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,17 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-	:storage => :dropbox,
-	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :path => ":style/:id_:filename"
+  end
 
-  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-	#validates_attachment_content_type line is necessary if you’re using the latest version of paperclip (v4.x).*
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates_attachment_presence :image
 end
